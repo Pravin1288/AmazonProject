a457057edd0e7f4124abf47388cbdafae3b2029b
unknown <pravinkumar12@gmail.com>
2024-09-28 21:50:06 +0100

    Adding Amazon Project

---
+++ AmazonPageObjectModel/src/test/java/Regression/LoginTest.java
@@ -0,0 +1,11 @@
+package Regression;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends SetUp{
+	@Test
+	public void ValidateWithCorrectUserName_PW() {
+		System.out.println("Validated Successfully");
+	}
+
+}
---
+++ AmazonPageObjectModel/src/test/java/Regression/SetUp.java
@@ -0,0 +1,48 @@
+package Regression;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class SetUp {
+
+	WebDriver driver;
+
+	//To Read the QA_URL, creating property file and reading the url from it
+	Properties prop = new Properties();
+	
+	@BeforeClass
+	public void Initialization() throws IOException {
+		
+		//To Read the File using File Input Stream
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\Properties\\config.properties");
+		
+		//To Load the File
+		prop.load(fis);
+		
+		//Open Chrome Browser
+		driver = new ChromeDriver();
+		
+		//Read the URL
+		driver.get(prop.getProperty("QA_Url"));
+		
+		//Maximize the Window
+		driver.manage().window().maximize();
+		
+		//Get the Title
+		System.out.println(driver.getTitle());		
+	}
+	
+	@AfterClass
+	
+	public void CleanUp() {
+		driver.quit();
+	}
+	
+}
