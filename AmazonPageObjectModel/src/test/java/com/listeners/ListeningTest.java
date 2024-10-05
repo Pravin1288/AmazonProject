bd9ae14d7667f701eac8951a670d34b137545cc6
unknown <pravinkumar12@gmail.com>
2024-10-05 23:10:28 +0100

    Framework_07_Adding_ExtentTestManager_Reports_Removed_the_COMMENTS

--- AmazonPageObjectModel/src/test/java/com/listeners/ListeningTest.java
+++ AmazonPageObjectModel/src/test/java/com/listeners/ListeningTest.java
@@ -12,60 +12,49 @@ import utility.ExtentTestManager;
 
 public class ListeningTest implements ITestListener {
 
-	
-	 public void onTestStart(ITestResult result) {
-		   	System.out.println("Running Test Method" + result.getMethod().getMethodName());
-		   	ExtentTestManager.startTest(result.getMethod().getMethodName());
-		  }
+	public void onTestStart(ITestResult result) {
+		System.out.println("Running Test Method" + result.getMethod().getMethodName());
+		ExtentTestManager.startTest(result.getMethod().getMethodName());
+	}
 
-		  public void onTestSuccess(ITestResult result) {
-		    // not implemented
-			  System.out.println("Test Execution Pass" + result.getMethod().getMethodName());
-			  ExtentTestManager.getTest().log(Status.PASS, "TEST PASSED");   
-		  }
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test Execution Pass" + result.getMethod().getMethodName());
+		ExtentTestManager.getTest().log(Status.PASS, "TEST PASSED");
+	}
 
-			/*
-			 * public void onTestFailure(ITestResult result) { // not implemented
-			 * System.out.println("Test Execution Failed" +
-			 * result.getMethod().getMethodName());
-			 * ExtentTestManager.getTest().log(Status.FAIL, "TEST FAILED"); }
-			 */
-		  
-		  //To fetch the Failure Exception (Getting the Error/Reason whatever FAILS)
-		  public void onTestFailure(ITestResult result) {
-			    // not implemented
-				  System.out.println("Test Execution Failed" + result.getMethod().getMethodName());
-				  ExtentTestManager.getTest().log(Status.FAIL  , result.getThrowable());
-			  }
+	/*
+	 * public void onTestFailure(ITestResult result) { // not implemented
+	 * System.out.println("Test Execution Failed" +
+	 * result.getMethod().getMethodName());
+	 * ExtentTestManager.getTest().log(Status.FAIL, "TEST FAILED"); }
+	 */
 
-		  public void onTestSkipped(ITestResult result) {
-		    // not implemented
-			  System.out.println("Test Execution Skipped" + result.getMethod().getMethodName());
-			  ExtentTestManager.getTest().log(Status.SKIP, "TEST SKIPPED");  
-		  }
+	// To fetch the Failure Exception (Getting the Error/Reason whatever FAILS)
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test Execution Failed" + result.getMethod().getMethodName());
+		ExtentTestManager.getTest().log(Status.FAIL, result.getThrowable());
+	}
 
-		  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		    // not implemented
-		  }
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test Execution Skipped" + result.getMethod().getMethodName());
+		ExtentTestManager.getTest().log(Status.SKIP, "TEST SKIPPED");
+	}
 
-		
-		  public void onTestFailedWithTimeout(ITestResult result) {
-		    onTestFailure(result);
-		    //This will automatically pick up the percentage
-		  }
-	
-		  public void onStart(ITestContext context) {
-		    // not implemented
-			  System.out.println("Test Suite" + context.getName() + "Started");
-		  }
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	}
 
-		
-		  public void onFinish(ITestContext context) {
-		    // not implemented
-			  System.out.println("Test Finish Suite" + context.getName() + "Finish");
-			  ExtentTestManager.endTest();
-			  ExtentManager.getInstance().flush();
-		  }
-		}
+	public void onTestFailedWithTimeout(ITestResult result) {
+		onTestFailure(result);
+		// This will automatically pick up the percentage
+	}
 
+	public void onStart(ITestContext context) {
+		System.out.println("Test Suite" + context.getName() + "Started");
+	}
 
+	public void onFinish(ITestContext context) {
+		System.out.println("Test Finish Suite" + context.getName() + "Finish");
+		ExtentTestManager.endTest();
+		ExtentManager.getInstance().flush();
+	}
+}
